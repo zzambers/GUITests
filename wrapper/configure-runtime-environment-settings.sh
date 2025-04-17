ff68497445e83fbd8d0640fead9f59b278db12c8
Zdenek Zambersky <zzambers@redhat.com>
2025-04-17 13:43:20 +0200

    Set TTL variable for GHA not to do manual run

--- wrapper/configure-runtime-environment-settings.sh
+++ wrapper/configure-runtime-environment-settings.sh
@@ -13,6 +13,9 @@ export SCRATCH_DISK=$(mktemp -d)
 
 # configure paths
 export TESTS_FOLDER="`dirname ${SCRIPT_DIR}`/tests"
+
+# set TTL not to do manual run
+export TTL=60
 }
 
 function configureDefaultSettings {
